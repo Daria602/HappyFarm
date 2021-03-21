8fcdd448ec291599a79438705978af906957935f
Daria602 <73795793+Daria602@users.noreply.github.com>
2021-03-22 00:43:38 +0200

    Update GameObject.h

--- GameObject.h
+++ GameObject.h
@@ -12,9 +12,34 @@ private:
 	std::string name;
 	int sellValue;
 	int buyValue;
+	friend std::ostream& operator<<(std::ostream& os, const GameObject& go_);
 
 public:
 	GameObject();
+	GameObject(std::string name, int sellValue, int buyValue);
 	virtual ~GameObject();
-};
+	GameObject(const GameObject& go_);
 
+	GameObject& operator=(const GameObject& another)
+	{
+		if (this != &another)
+		{
+			this->name = another.name;
+			this->sellValue = another.sellValue;
+			this->buyValue = another.buyValue;
+
+		}
+
+		return *this;
+	}
+
+	void initGameObject(std::string name, int buyValue );
+
+	std::string getName();
+	int getSellValue();
+	int getBuyValue();
+
+	void setName(std::string name);
+	void setSellValue(int sellValue);
+	void setBuyValue(int buyValue);
+};
